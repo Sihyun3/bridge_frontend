9185819490344de34c13853496ed3b0b2cab44e0
soyoung <sy.park.fe@gmail.com>
2023-04-12 18:13:56 +0900

    0412_soyoung

--- src/App.js
+++ src/App.js
@@ -1,45 +1,15 @@
-import logo from './logo.svg';
-import Header from './Header/Header';
-import Layout from './Layout/Layout';
+import Header2 from "./Header/Header2";
 
-// import logo from './logo.svg';
-import './App.css';
-import Main from './Main/Main';
-import Login from './Login/Login'
 
-import SignUp from './SignUp/SignUp';
-import TipList from './TipList/TipList';
-import MainAd from './Administrator/MainAd';
-
-// import SignUp from './SignUp/SignUp';
-// import TipList from './TipList/TipList';
-import ReportPage from './Report/ReportPage';
-import SignUp from './SignUp/SignUp';
-import TipList from './TipList/TipList';
-import Notice from './Admin-Notice/Notice';
 
 
 
 function App() {
   return (
     <>
-
-    {/* <Main/>
-    <Login/> */}
-    <MainAd/>
-
-      <Layout />
+      <Header2/>
 
 
-      <Main />
-      <Login />
-      <ReportPage/>
-      {/* <SignUp/> */}
-      {/* <TipList/> */}
-    {/* <Login/> */}
-    {/* <SignUp/> */}
-    {/* <TipList/> */}
-    <Notice/>
     </>
         )
   }
