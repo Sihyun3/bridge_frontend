762103320f790ca12f2923690b8d86f1d83ec8b8
Sihyun <yellownyou@gmail.com>
2023-04-12 13:06:45 +0900

    0412_sihyun

--- src/App.js
+++ src/App.js
@@ -1,18 +1,10 @@
 import logo from './logo.svg';
 import Header from './Header/Header';
 import Layout from './Layout/Layout';
-
-// import logo from './logo.svg';
 import './App.css';
 import Main from './Main/Main';
 import Login from './Login/Login'
-
-import SignUp from './SignUp/SignUp';
-import TipList from './TipList/TipList';
 import MainAd from './Administrator/MainAd';
-
-// import SignUp from './SignUp/SignUp';
-// import TipList from './TipList/TipList';
 import ReportPage from './Report/ReportPage';
 import SignUp from './SignUp/SignUp';
 import TipList from './TipList/TipList';
