3fe275bbf2bb9c41c16cdc031b299b5a59866a75
zgsda <121409809+zgsda@users.noreply.github.com>
2023-04-12 14:25:49 +0900

    0412_jimin

--- src/Admin-Notice/Notice.js
+++ src/Admin-Notice/Notice.js
@@ -1,5 +1,5 @@
 import style from './Notice.module.css'
-import searchImg from 'C:/tantara/tantara_frontend/src/TipList/searchImg.png'
+import searchImg from 'C:/bridge/bridge_frontend/src/Admin-Notice/searchImg.png'
 
 const Notice = () => {
     return (
