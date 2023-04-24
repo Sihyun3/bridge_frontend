7fdaa8d4a113bf4c6392f71b024ea22117a9cb10
soyun <swell1402@naver.com>
2023-04-24 11:52:48 +0900

    0424_soyun

--- src/Jam/JamDetail2.js
+++ src/Jam/JamDetail2.js
@@ -1,12 +1,41 @@
 import { useRef, useState } from "react";
 import axios from "axios";
-import Waveform from "./Waveform";
+import Waveform from "./Waveform2";
 
 export default function JamDetail2() {
   const [value, setvalue] = useState([]);
   const [data, setData] = useState([]);
   const child = useRef([]);
 
+  // 코맨트
+  const [comment, setComment] = useState('');
+  const [writer, setWriter] = useState('');
+
+  const handleChangeComment = (e) => {
+    setComment(e.target.value);
+  };
+
+  const handleChangeWriter = e => {
+    setWriter(e.target.value);
+  };
+
+  const handleCommentSubmit = (e) => {
+    e.preventDefault();
+    console.log(`Comment: ${comment}`);
+    //cIdx 부분 5번으로 하드코딩 
+    axios.post(`http://localhost:8080/api/insertComments/5`, { "userId":writer, "contents": comment  })
+      .then(response => {
+        console.log(response);
+        alert('코맨트가 정상적으로 등록되었습니다')
+
+      })
+      .catch(error => {
+        console.log(error);
+        alert(`오류가 발생했습니다 (${error.message})`);
+      });
+  };
+
+  // 파일 업로드
   const onSubmit = (e) => {
     e.preventDefault();
     let files = e.target.profile_files.files;
@@ -14,7 +43,6 @@ export default function JamDetail2() {
     for (let i = 0; i < files.length; i++) {
       formData.append("files", files[i]);
     }
-
     axios({
       method: 'POST',
       url: `http://localhost:8080/api/insertmusic`,
@@ -40,6 +68,7 @@ export default function JamDetail2() {
     }
   }
 
+  // 전체 재생 (자식에서 부모로)
   const allplay = () => {
     value.forEach((index) => {
       child.current[index].PlayAll();
@@ -53,9 +82,11 @@ export default function JamDetail2() {
         <span>전체 선택</span>
       </div>
 
+      {/* 파형&체크박스 맵 돌린거 */}
       {data.map((musicInfo, index) => {
         return (
           <div key={musicInfo.musicUUID}>
+            {/* 체크박스 */}
             <input
               type="checkbox"
               checked={value.includes(index)}
@@ -67,29 +98,47 @@ export default function JamDetail2() {
                 }
               }}
             />
+            {/* 파형 */}
             <Waveform
               key={musicInfo.musicUUID}
               src={`http://localhost:8080/api/getMusic/${musicInfo.musicUUID}`}
               ref={(elem) => (child.current[index] = elem)}
             />
+            {/* 노래제목 */}
             {musicInfo.musicTitle}
           </div>
         );
-      })}
+      })}  {/* 맵 끝*/}
 
-      <br />
       <button onClick={allplay}>All Play/Pause</button>
-
-      <br />
-      <hr />
-      <br />
 
       <form onSubmit={(e) => onSubmit(e)}>
         <input type="file" name="profile_files" multiple="multiple" />
         <button type="submit">제출</button>
       </form>
+
+      <form onSubmit={handleCommentSubmit}>
+        <input type="text" id="writer" name="writer" value={writer} onChange={handleChangeWriter} placeholder="작성자" />
+        <input type="text" id="comment" name="comment" value={comment} onChange={handleChangeComment} placeholder="코맨트를 입력하세요" />
+        <button type="submit">작성</button>
+      </form>
+
+      {/* <div className="one-line-list"> */}
+        {/* <ul className="line-list"> */}
+          {/* 코맨트 리스트 데이터 출력 */}
+          {/* {
+            data.map((n) => (
+              <li key={n.commentIdx}>
+                <div>
+                  <span className="whatname">작성자 {n.writer}</span>
+                </div>
+                <div className="review_info">{n.contents}</div>
+              </li>
+            ))
+          }
+        </ul>
+      </div> */}
+      
     </>
   );
 }
-
-
