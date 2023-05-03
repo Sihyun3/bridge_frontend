600feb10cd6597512698431096ce3386e0e63a9c
deepin <deepin3809@gmail.com>
2023-05-03 10:25:56 +0900

    0505_ara

--- src/Admin-Notice/NoticeDetail.js
+++ src/Admin-Notice/NoticeDetail.js
@@ -1,22 +1,115 @@
-import style from './NoticePage.module.css';
-// import NoticePage from 'react';
-import '../reset.css';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import style from './NoticeWrite.module.css';
+// import style from './Notice.module.css';
 
+function NoticeDetail({match, history}) {
 
-function NoticeDetail() {
+    const {noticeIdx} = match.params;
+
+    const [datas, setDatas] = useState([]);
+    const [notice, setNotice] = useState({});
+    const [title, setTitle] = useState('');
+    const [contents, setContents] = useState('');
+    
+    const handlerChangeTitle = e => setTitle(e.target.value);
+    const handlerChangeContents = e => setContents(e.target.value);
+
+    
+
+    useEffect(() => {
+        sessionStorage.setItem("token",'eyJhbGciOiJIUzI1NiJ9.eyJuYW1lIjoiY2FyIiwiZW1haWwiOiJhYWFAYWFhLmFhYSIsInN1YiI6ImFkbWluIiwianRpIjoiNWI5NjRiNjUtYTFjZC00ZTg2LTk1ZWUtYjBiZjExZDU1ZGM1IiwiaWF0IjoxNjgyNjY5NDA3LCJleHAiOjEwMzIyNjY5NDA3fQ.2Wmjibk8x_wdkaUyt28vETqI3E2zbQQDTSqF9BFkSjk');
+        axios.get(`http://localhost:8080/api/notice/detail/${noticeIdx}` , {headers: {'Authorization': `Bearer ${sessionStorage.getItem('token')}`}})
+        .then(reponse => {
+            console.log(reponse);
+            setNotice(reponse.data);
+                setTitle(reponse.data.title);
+                setContents(reponse.data.contents);
+        })
+        .catch(error => console.log(error));
+    },[]);
+
+    const handlerClickList = () => {
+        console.log(history);
+        history.push('/notice');
+    };
+
+    const handlerClickUpdate = () => {
+        axios.put(`http://localhost:8080/api/notice/update/${noticeIdx}`,
+        { "title": title, "contents": contents }) 
+        .then(response => {
+            console.log(response);
+            if(response.data === 1) {
+                alert('수정 완료');
+            } else {
+                alert('수정 실패');
+                return;
+            }
+        })
+        .catch(error => {
+            console.log(error);
+        console.log(error);
+            alert(`수정에 실패했습니다.(${error.message})`)
+            return;
+        });
+    };
+
+    const handlerClickDelete = () => {
+        axios.delete(`http://localhost:8080/api/notice/delete/${noticeIdx}`)
+        .then(response => {
+            console.log(response);
+            if(response.data === 1) {
+                alert('해당 글이 정상적으로 삭제되었습니다.');
+                history.push('/notice');
+            } else {
+                alert('삭제에 실패했습니다. 다시 시도해주세요.');
+                return;
+            }
+        })
+        .catch(error => {
+            console.log(error);
+            alert(`삭제에 실패하였습니다.(${error.message})`);
+            return;
+        });
+    };
+
+  
     return (
-      <>
+        <>
         <h1 className={style.Notice}>공지사항 등록</h1>
         <div className={style.Box}>
+
           <h1 className={style.Title}>제목</h1>
-          <input className={style.TitleBox} />
+          {/* <input type="text" id="title" name="title" value={notice.title} onChange={handlerChangeTitle}/> */}
+            <input className={style.TitleBox} value={notice.title} onChange={handlerChangeTitle}/>
+          {/* <div className={style.TitleBox}>{notice.title}</div> */}
           <h1 className={style.Contents}>내용</h1>
-          <input className={style.ContentsBox} />
-          <button className={style.Button2} onClick=''>수정</button>
-          <button className={style.Button3} onClick=''>삭제</button>
+          <input className={style.ContentsBox} value={notice.contents} onChange={handlerChangeContents}/>
+          {/* <textarea className={style.TitleBox} value={notice.contents} onChange={handlerChangeContents}></textarea> */}
+          <button className={style.Button2} onClick={handlerClickUpdate}>수정</button>
+          <button className={style.Button3} onClick={handlerClickDelete}>삭제</button>
+          <button className={style.Button3} onClick={handlerClickList}>목록으로</button>
         </div>
+
+        { datas && datas.map(notice => (
+                <div className={style.TitleBox}>
+                    <div key={notice.title}>
+                        <div>{notice.title}</div>
+                        </div>
+                        <div key={notice.ContentsBox}>
+                        <div>{notice.contents}</div>
+                        </div>
+                        
+                        </div>
+                        ))
+        }
+                        {/* <div className="title">
+                            <Link to={`/notice/detail/${notice.noticeIdx}`}>{notice.title}</Link></td> */}
+                           {/* <Link to={`/notice/detail/${notice.noticeIdx}`}>{notice.title}</Link></div> */}
+        
       </>
     );
-  }
+};
+
 
 export default NoticeDetail;
--- src/App.js
+++ src/App.js
@@ -22,6 +22,9 @@ import Payment from './Payment/Payment';
 import Chatting from './Chatting/Chatting';
 import ProfileWrite from './Profile/ProfileWrite';
 import JamWrite from './Jam/JamWrite';
+import NoticeWrite from './Admin-Notice/NoticeWrite';
+import NoticeDetail from './Admin-Notice/NoticeDetail';
+
 
 // 모듈
 import { Route } from 'react-router-dom';
@@ -29,40 +32,42 @@ import { Route } from 'react-router-dom';
 function App() {
   return (
     <>
-      <Header2 />
-      <Route path="/" component={LoginStart} exact={true} />
+      {/* <Header2 /> */}
+      {/* <Route path="/" component={LoginStart} exact={true} /> */}
 
       {/* 메인, 공지 */}
-      <Route path="/1" component={Main} exact={true} />
+      {/* <Route path="/1" component={Main} exact={true} /> */}
       <Route path="/2" component={Notice} exact={true} />
+      
       {/* 로그인, 회원가입 */}
-      <Route path="/3" component={Login} exact={true} />
-      <Route path="/4" component={SignUp} exact={true} />
+      {/* <Route path="/3" component={Login} exact={true} /> */}
+      {/* <Route path="/4" component={SignUp} exact={true} /> */}
       {/* 관리자 */}
-      <Route path="/5" component={ReportPage} exact={true} />
-      <Route path="/6" component={ReportDetail} exact={true} />
-      <Route path="/7" component={DealListAd} exact={true}/>
-      <Route path="/8" component={MainAd} exact={true}/>
+      {/* <Route path="/5" component={ReportPage} exact={true} />
+      <Route path="/6" component={ReportDetail} exact={true} /> */}
+      {/* <Route path="/7" component={DealListAd} exact={true}/> */}
+      {/* <Route path="/8" component={MainAd} exact={true}/> */}
       {/* 프로필 */}
-      <Route path="/9" component={ProfileWrite} exact={true}/>
-      <Route path="/10" component={ProfileDetail} exact={true} />
+      {/* <Route path="/9" component={ProfileWrite} exact={true}/> */}
+      {/* <Route path="/10" component={ProfileDetail} exact={true} /> */}
       {/* 팁 */}
-      <Route path="/11" component={TipList} exact={true} />
+      {/* <Route path="/11" component={TipList} exact={true} /> */}
       {/* 잼 */}
-      <Route path="/12" component={JamList} exact={true} />
-      <Route path="/13" component={JamWrite} exact={true} />
+      {/* <Route path="/12" component={JamList} exact={true} /> */}
+      {/* <Route path="/13" component={JamWrite} exact={true} /> */}
       {/* 파트너 구인 */}
-      <Route path="/14" component={PartnerWrite} exact={true}/>
-      <Route path="/15" component={Chatting} exact={true}/>
-      <Route path="/16" component={Payment} exact={true}/>
-      <Route path="/17" component={Doing} exact={true}/>
-      <Route path="/18" component={Charge} exact={true}/>
+      {/* <Route path="/14" component={PartnerWrite} exact={true}/> */}
+      {/* <Route path="/15" component={Chatting} exact={true}/> */}
+      {/* <Route path="/16" component={Payment} exact={true}/> */}
+      {/* <Route path="/17" component={Doing} exact={true}/> */}
+      {/* <Route path="/18" component={Charge} exact={true}/> */}
 
       {/* 비었음 사용할 것 */}
-      <Route path="/19" component={Notice} exact={true} />
-      <Route path="/20" component={Notice} exact={true} />
+      <Route path="/notice/write" component={NoticeWrite} exact={true} />
+      <Route path="/notice" component={Notice} exact={true} />
+      <Route path="/notice/detail/:noticeIdx" component={NoticeDetail} exact={true} />
 
-      <Footer />
+      {/* <Footer /> */}
     </>
   )
 }
