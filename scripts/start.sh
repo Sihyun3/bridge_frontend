99a6e938409f8aea5a768ba80972fe2c05e67124
Sihyun3 <121409925+Sihyun3@users.noreply.github.com>
2023-05-18 17:58:58 +0900

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -10,13 +10,13 @@ DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
 TIME_NOW=$(date +%c)
 
 
-cp $PROJECT_ROOT/conf.d /
+cp $PROJECT_ROOT/nginx.conf /etc/nginx/conf.d/
 
 cp $PROJECT_ROOT/build /usr/share/nginx/html/
 echo "$TIME_NOW : copy build" >> $DEPLOY_LOG
 
 
 echo "$TIME_NOW : run nginx" >> $DEPLOY_LOG
-nginx -g daemon off
+sudo service nginx start
 # chmod 111 JAR_FILE
 # nohup java -jar $JAR_FILE > $APP_LOG 2> $ERROR_LOG &
